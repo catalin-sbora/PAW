e584efa80c07b86efc5fdcc1bbd37fe3b79b66e2
catalin.sbora <catalin.sbora@gmail.com>
2015-10-13 06:16:32 -0400

    updates for the login form

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if ($_SESSION['loggedin'] != 1)
+{
+	/*
+		if not logged in just redirect to login.html 
+	*/
+	$serverName = $_SERVER['HTTP_HOST'];
+	$url = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+	$extra = 'login.html';  // change accordingly
+	header('Location: '.$extra, true, 303);
+	die();
+}
+/*
+at this point it means that the user is already logged in so just continue with html for index
+*/
+?>
+<html>
+	<head>		
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet" >
+	</head>
+	
+	<body>		
+		<div class="panel panel-body">
+		<h2>
+			Hello World	
+		</h2>
+		</div>
+		<script src="js/libs/jquery-1.11.0.min.js"/>
+		<script src = "js/libs/bootstrap.js" />
+		<script src = "js/paw.js"/>
+	</body>
+</html
